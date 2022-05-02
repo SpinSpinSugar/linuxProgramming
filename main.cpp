fd5bbb32e798c6c0a5ca526d0cd8193acafe289a
SpinSpinSugar <88590354+SpinSpinSugar@users.noreply.github.com>
2022-05-02 22:58:45 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
-#include "ArrayList.h"
-#include "ArrayList.cpp"
+#include "LinkedList.h"
 
 int main(int argc, char* argv[]) {
-    ArrayList c(2, 2);
-    c.push_back(11);
-    c.push_front(11);
-    c.insert(2, 5);
-    c.insert(0, 5);
-    c.resize(10);
-    c.insert(50, 50);
-    //c.erase(4);
-    //c.push_front(5);
-    ArrayList d = c;
-    //c.erase(2);
-    //ArrayList c;
-    /*
-    c.push_back(515155);
-    c.push_back(123);
-    c.push_back(234);
-    ArrayList d = c;
-    c[0] = 222;
-    d[0] = 111;
-    c.push_front(333);
-    c.push_front(444);
-    c.pop_front();
-    d.pop_back();
-    std::cout << c << c.size() << ' ' << c.capacity() << '\n';
-    c.insert(2, 5);
-    std::cout << c << c.size() << ' ' << c.capacity() << '\n';
-    c.insert(0, 999);
-    std::cout << c << '\n';
-    c.erase(0);
-    std::cout << c << c.size() << ' ' << c.capacity() << '\n';
-    std::cout << d << d.size() << ' ' << d.capacity() << '\n';
-    std::cout << c[0];
-    std::cout << c << '\n';
-    */
-    std::cout << c << '\n';
-    c.sort();
-    std::cout << c << '\n';
-    std::cout << c.size() << ' ' << c.capacity();
-    return EXIT_SUCCESS;
+	LinkedList a;
+	a.push_front(7);
+	a.push_front(6);
+	a.push_front(5);
+	a.push_front(4);
+	a.push_front(3);
+	a.push_front(2);
+	a.push_front(1);
+	a.push_front(0);
+	a.push_back(8);
+	a.push_back(9);
+	a.push_back(10);
+	a.insert(10, 777);
+	std::cout << a << '\n';
+	a.swap(2, 5);
+	a.swap(2, 13); //do nothing
+	a.insert(12, 50); //do nothing
+	a.erase(12); //do nothing
+	std::cout << a << '\n' << a.front() << '\n' << a.back() << '\n';
+	return EXIT_SUCCESS;
 }
